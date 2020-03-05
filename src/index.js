215062a1b0d82ee7aafdf58274b66434b2b781e6
Eldar <eldar.hidirow@gmail.com>
2020-03-05 14:20:55 +0300

    Осталось доделать require и подменю

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './scss/base.scss';
+import './partials/main/main.js';
+import './partials/header/header.js';
---
+++ src/partials/main/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.main__container-elem, .main__confirmation, .main__flex-elem').click(function () {
+        $(this).toggleClass('active');
+    });
+})
